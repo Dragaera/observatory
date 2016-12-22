eef958305e88961cdbccca4c4eeb032126851d46
Michael Senn <michael@senn.email>
2016-12-22 16:57:52 +0100

    Adds FormatHelper.

---
+++ app/helpers/format_helper.rb
@@ -0,0 +1,48 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Observatory
+  class App
+    module FormatHelperHelper
+      SECONDS_PER_MINUTE = 60
+      SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60
+      SECONDS_PER_DAY = SECONDS_PER_HOUR * 24
+
+      def pp_timespan(seconds)
+        if seconds >= SECONDS_PER_DAY
+          days = seconds / SECONDS_PER_DAY
+          seconds -= days * SECONDS_PER_DAY
+        else
+          days = 0
+        end
+
+        if seconds >= SECONDS_PER_HOUR
+          hours = seconds / SECONDS_PER_HOUR
+          seconds -= hours * SECONDS_PER_HOUR
+        else
+          hours = 0
+        end
+
+        if seconds >= SECONDS_PER_MINUTE
+          minutes = seconds / SECONDS_PER_MINUTE
+          seconds -= minutes * SECONDS_PER_MINUTE
+        else
+          minutes = 0
+        end
+
+        out = []
+        out << "#{ days }d" if days > 0
+        out << "#{ hours }h" if hours > 0
+        out << "#{ minutes }m" if minutes > 0
+        out << "#{ seconds }s" if seconds > 0
+
+        out.join(' ')
+      end
+
+      def pp_separator(number, sep = "'")
+        number.to_s.reverse.gsub(/\d\d\d(?=\d)/) { |s| "#{ s }#{ sep }" }.reverse
+      end
+    end
+
+    helpers FormatHelperHelper
+  end
+end
