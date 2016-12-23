ba822ce37330e53728a855d6a55db9a76dbb1db5
Michael Senn <michael@senn.email>
2016-12-23 13:19:51 +0100

    Adds flash messages in case of error.

--- app/controllers/player_data.rb
+++ app/controllers/player_data.rb
@@ -5,11 +5,12 @@ Observatory::App.controllers :player_data do
   get :single do
     if params.key? 'steam_account_id'
       @data = get_player_data(params.fetch('steam_account_id'))
+      # `nil` indicates that something went awry with the request.
+      redirect(url(:player_data, :single)) unless @data
     end
     render 'single'
   end
 
   get :multiple do
-
   end
 end
--- app/helpers/player_data_helper.rb
+++ app/helpers/player_data_helper.rb
@@ -4,14 +4,18 @@ module Observatory
   class App
     module PlayerDataHelper
       def get_player_data(steam_id)
+        # Todo: Settingi flash messages in a helper feels dirty.
+        # And special return values (nil here) seems an awful lot like C. ;)
         stalker = HiveStalker::Stalker.new()
-        begin
-          @data = stalker.get_player_data(steam_id)
-        rescue HiveStalker::APIError => e
-          msg = "Caught API Error: #{ e.message }"
-          msg << " caused by: #{ e.cause.message }" if e.cause
-          raise e
-        end
+        stalker.get_player_data(steam_id)
+      rescue ArgumentError => e
+        flash[:error] = e.message
+        nil
+      rescue HiveStalker::APIError => e
+        msg = e.message
+        msg << " caused by: #{ e.cause.message }" if e.cause
+        flash[:error] = msg
+        nil
       end
     end
 
