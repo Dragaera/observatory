e248e5a3ea7188132bcf7ef639058316bc402589
Michael Senn <michael@senn.email>
2016-12-31 17:55:59 +0100

    Changes default pagination size to 30.

--- config/observatory.rb
+++ config/observatory.rb
@@ -1,6 +1,6 @@
 module Observatory
   module Config
-    PAGINATION_SIZE_LEADERBOARD = ENV.fetch('PAGINATION_SIZE_LEADERBOARD', 15).to_i
+    PAGINATION_SIZE_LEADERBOARD = ENV.fetch('PAGINATION_SIZE_LEADERBOARD', 30).to_i
     if PAGINATION_SIZE_LEADERBOARD < 1
       raise ArgumentError, "PAGINATION_SIZE_LEADERBOARD must be greater than 0. Was: #{ PAGINATION_SIZE_LEADERBOARD }"
     end
