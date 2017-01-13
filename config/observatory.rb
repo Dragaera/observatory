9afca84db21767eb038f7c0f251293e0a3db806b
Michael Senn <michael@senn.email>
2017-01-13 07:40:53 +0100

    Disallows PLAYER_DATA_UPDATE_INTERVAL to be 0.

--- config/observatory.rb
+++ config/observatory.rb
@@ -9,9 +9,8 @@ module Observatory
     REDIS_PORT = ENV.fetch('REDIS_PORT', 6379).to_i
     RESQUE_WEB_PATH = ENV.fetch('RESQUE_WEB_PATH', nil)
 
-    # Interval for automatic player data updates in hours.
     PLAYER_DATA_UPDATE_INTERVAL = ENV.fetch('PLAYER_DATA_UPDATE_INTERVAL', 24).to_i
-    if PLAYER_DATA_UPDATE_INTERVAL <= 0
+    if PLAYER_DATA_UPDATE_INTERVAL < 0
       raise ArgumentError, "PLAYER_DATA_UPDATE_INTERVAL must be greater than 0. Was: #{ PLAYER_DATA_UPDATE_INTERVAL }"
     end
   end
