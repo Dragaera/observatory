f5f615cb4b6e148acf5c4605c7b4db104151f6cd
Michael Senn <michael@senn.email>
2017-01-16 21:02:05 +0100

    Prevent user from scheduling multiple updates for the same player.

--- app/controllers/players.rb
+++ app/controllers/players.rb
@@ -8,10 +8,12 @@ Observatory::App.controllers :players do
   post :update, map: '/player/:id/update' do |id|
     @player = get_or_404(Player, id)
 
-    if @player.async_update_data
-      flash[:success] = 'Scheduled player update'
+    if @player.update_scheduled_at
+      flash[:error] = 'There is already an updated scheduled for this player, please be patient.'
+    elsif @player.async_update_data
+      flash[:success] = 'Scheduled player update.'
     else
-      flash[:error] = 'Failed to schedule player update'
+      flash[:error] = 'Failed to schedule player update.'
     end
 
     redirect(url(:players, :profile, id: id))
