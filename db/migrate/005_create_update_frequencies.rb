5bbcbf54829fde9583f3c0f7ecceeb0be134300d
Michael Senn <michael@senn.email>
2017-01-16 22:36:24 +0100

    Adds additional flags to UpdateFrequency.
    - Threshold: Number of seconds within which a player must have had new
      data to be assigned that update frequency.
    - Fallback: Flag to mark which frequency to use as a fallback for
      inactive players.

--- db/migrate/005_create_update_frequencies.rb
+++ db/migrate/005_create_update_frequencies.rb
@@ -3,15 +3,17 @@ Sequel.migration do
     create_table :update_frequencies do
       primary_key :id
 
-      varchar :name,     null: false, unique: true
-      integer :interval, null: false
-      bool    :enabled,  null: false, default: true
+      varchar :name,      null: false, unique: true
+      integer :interval,  null: false
+      integer :threshold, null: false
+      bool    :enabled,   null: false, default: true
+      bool    :fallback,  null: false, default: false
     end
 
-    from(:update_frequencies).insert(name: 'Hourly', interval: 60 * 60)
-    daily  = from(:update_frequencies).insert(name: 'Daily', interval: 24 * 60 * 60)
-    from(:update_frequencies).insert(name: 'Weekly', interval: 7 * 24 * 60 * 60)
-    from(:update_frequencies).insert(name: 'No Updates', interval: 0, enabled: false)
+    from(:update_frequencies).insert(name: 'Hourly', interval: 60 * 60, threshold: 60 * 60 * 24)
+    daily  = from(:update_frequencies).insert(name: 'Daily', interval: 24 * 60 * 60, threshold: 60 * 60 * 24 * 7)
+    from(:update_frequencies).insert(name: 'Weekly', interval: 7 * 24 * 60 * 60, threshold: 60 * 60 * 24 * 365, fallback: true)
+    from(:update_frequencies).insert(name: 'No Updates', interval: 0, enabled: false, threshold: 0)
 
     alter_table :players do
       add_foreign_key :update_frequency_id, :update_frequencies, null: false, default: daily
