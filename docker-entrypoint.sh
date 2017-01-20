ab966a08328cc87669fe6ce60c6795cf4dd2c4f9
Michael Senn <michael@senn.email>
2017-01-20 20:38:51 +0100

    Docker entrypoint: Adds CI task, waits for database.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,35 +1,55 @@
 #!/bin/bash
 
-case "$1" in
-    application)
-        echo "Starting application server..."
-        exec unicorn -c unicorn.conf.rb
-        ;;
-    worker)
-        echo "Starting Resque worker..."
-        echo "Listening to queues: '$QUEUE'"
-        echo "Polling interval: $INTERVAL"
-        exec rake resque:work
-        ;;
-    scheduler)
-        echo "Starting Resque scheduler..."
-        echo "Scheduling interval: $RESQUE_SCHEDULER_INTERVAL"
-        exec rake resque:scheduler
-        ;;
-    rake)
-        echo "Calling Rake task $2"
-        exec rake $2
-        ;;
-    migrate)
-        echo "Applying database migrations"
-        exec rake sq:migrate
-        ;;  
-    shell)
-        echo "Opening Padrino shell"
-        exec padrino c
-        ;;
-    *)
-        echo "Don't know what to do with $1"
-        echo "Valid commands: application, rake, shell"
+function main {
+    _wait_for_database
+
+    case "$1" in
+        application)
+            echo "Starting application server..."
+            exec unicorn -c unicorn.conf.rb
+            ;;
+        worker)
+            echo "Starting Resque worker..."
+            echo "Listening to queues: '$QUEUE'"
+            echo "Polling interval: $INTERVAL"
+            exec rake resque:work
+            ;;
+        scheduler)
+            echo "Starting Resque scheduler..."
+            echo "Scheduling interval: $RESQUE_SCHEDULER_INTERVAL"
+            exec rake resque:scheduler
+            ;;
+        rake)
+            echo "Calling Rake task $2"
+            exec rake $2
+            ;;
+        migrate)
+            echo "Applying database migrations"
+            exec rake sq:migrate
+            ;;  
+        shell)
+            echo "Opening Padrino shell"
+            exec padrino c
+            ;;
+        ci-spec)
+            echo "Executing CI specs"
+            exec rake ci:spec
+            ;;
+        *)
+            echo "Don't know what to do with $1"
+            echo "Valid commands: application, worker, scheduler, rake, migrate, shell, ci-spec"
+            exit 1
+    esac
+}
+
+function _wait_for_database {
+    ./wait-for-it.sh $DB_HOST:$DB_PORT --timeout=20
+    if [ $? -eq 0 ]; then
+        echo "Database ready"
+    else
+        echo "Databse not ready in time"
         exit 1
-esac
+    fi
+}
+
+main $@
