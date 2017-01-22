774329010bc8cacdd78419b58de67af1d496a497
Michael Senn <michael@senn.email>
2017-01-22 18:17:47 +0100

    Moves graph titles into graphs, adds axis titles.

--- app/helpers/player_graphs_helper.rb
+++ app/helpers/player_graphs_helper.rb
@@ -6,18 +6,33 @@ module Observatory
       def playtime_graph(player)
         area_chart(
           url(:player_graphs, :playtime, player_id: player.id),
+          ytitle: 'Playtime [h]',
           stacked: true,
           curve: false,
           download: "playtime_#{ player.account_id }",
+          library: {
+            title: {
+              display: true,
+              text: 'Playtime',
+            }
+          }
         )
       end
 
       def skill_graph(player)
         line_chart(
           url(:player_graphs, :skill, player_id: player.id),
+          ytitle: 'Skill',
           curve: false,
           label: 'Skill',
           download: "skill_#{ player.account_id }",
+          legend: false,
+          library: {
+            title: {
+              display: true,
+              text: 'Skill',
+            }
+          }
           # https://github.com/ankane/chartkick.js/issues/79
           # library: {
           #   scales: {
