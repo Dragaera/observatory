b84b10f2368db7f896f26ac5e896a8a38ee64688
Michael Senn <michael@senn.email>
2017-02-17 18:17:17 +0100

    Create specialized index for rank query which encocmpasses all columns.
    As it is more efficient to perform one query instead of four, it makes
    sense to have the index cover all four ranked columns.

--- db/migrate/020_add_index_for_player_rankings.rb
+++ db/migrate/020_add_index_for_player_rankings.rb
@@ -1,19 +1,27 @@
 Sequel.migration do
   up do
     alter_table :player_data_points do
-      add_index [:id, Sequel.desc(:skill), :player_id]
-      add_index [:id, Sequel.desc(:experience), :player_id]
-      add_index [:id, Sequel.desc(:score), :player_id]
-      add_index [:id, Sequel.desc(:score_per_second), :player_id]
+      add_index [
+        :id,
+        Sequel.desc(:skill),
+        Sequel.desc(:experience),
+        Sequel.desc(:score),
+        Sequel.desc(:score_per_second),
+        :player_id
+      ]
     end
   end
 
   down do
     alter_table :player_data_points do
-      drop_index [:id, Sequel.desc(:skill), :player_id]
-      drop_index [:id, Sequel.desc(:experience), :player_id]
-      drop_index [:id, Sequel.desc(:score), :player_id]
-      drop_index [:id, Sequel.desc(:score_per_second), :player_id]
+      drop_index [
+        :id,
+        Sequel.desc(:skill),
+        Sequel.desc(:experience),
+        Sequel.desc(:score),
+        Sequel.desc(:score_per_second),
+        :player_id
+      ]
     end
   end
 end
