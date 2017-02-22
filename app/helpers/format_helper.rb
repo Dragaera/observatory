619c42ff7293a303c5b28ca3ed481f9281c12800
Michael Senn <michael@senn.email>
2017-02-22 18:55:40 +0100

    Port over `#pp_form_errors`

--- app/helpers/format_helper.rb
+++ app/helpers/format_helper.rb
@@ -57,6 +57,12 @@ module Observatory
           raise ArgumentError, "#{ input.inspect } could not be interpreted as boolean"
         end
       end
+
+      def pp_form_errors(form_errors)
+        form_errors.map do |attr, errors|
+          "#{ attr.capitalize }: #{ errors.map(&:capitalize).join(', ') }"
+        end
+      end
     end
 
     helpers FormatHelperHelper
