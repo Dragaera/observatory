79675d67136d8473479eff534a047aaca701b230
Michael Senn <michael@senn.email>
2017-02-22 18:26:10 +0100

    Add controller action to udpate update frequencies.

--- app/controllers/update_frequencies.rb
+++ app/controllers/update_frequencies.rb
@@ -14,4 +14,17 @@ Observatory::App.controllers :update_frequencies do
     render 'edit'
   end
 
+  post :edit, map: '/update_frequencies/:id/edit' do
+    frequency = get_or_404(UpdateFrequency, params['id'])
+    data = params['update_frequency']
+    frequency.update(
+      name: data.fetch('name'),
+      threshold: data.fetch('threshold'),
+      interval: data.fetch('interval'),
+      enabled: to_bool(data.fetch('enabled')),
+      fallback: to_bool(data.fetch('fallback')),
+    )
+
+    redirect(url(:update_frequencies, :index))
+  end
 end
