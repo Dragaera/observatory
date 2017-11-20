73f96f37750ef0d625078dec671f586f461b6da7
Michael Senn <michael@senn.email>
2017-11-20 11:50:39 +0100

    Add NCT 2017 badges.

--- db/migrate/023_add_steam_inventory_badges.rb
+++ db/migrate/023_add_steam_inventory_badges.rb
@@ -12,10 +12,19 @@ Sequel.migration do
 
       add_index  [:key, :type], unique: true
     end
+
+    group_nct_17 = from(:badge_groups).insert(name: 'NCT 2017', sort: 7)
+    [
+      [1, 'NCT 2017 Gold',   'nct_17_gold.png',   '2167766106', group_nct_17],
+      [2, 'NCT 2017 Silver', 'nct_17_silver.png', '2167564054', group_nct_17], [3, 'NCT 2017 Blue',   'nct_17_blue.png',   '2167567673', group_nct_17],
+    ].each do |ary|
+      from(:badges).insert(sort: ary[0], name: ary[1], image: ary[2], key: ary[3], badge_group_id: ary[4], type: 'steam')
+    end
   end
 
   down do
     from(:badges).where(type: 'steam').delete
+    from(:badge_groups).where(name: ['NCT 2017']).delete
 
     alter_table :badges do
       drop_index [:key, :type]
